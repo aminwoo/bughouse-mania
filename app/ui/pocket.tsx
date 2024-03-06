384c8515112f51e214e8f9b8c3241f04dfa7472f
Ben <bwoo2718@gmail.com>
2024-03-07 00:18:02 +1100

    Add pocket count

--- app/ui/pocket.tsx
+++ app/ui/pocket.tsx
@@ -14,7 +14,7 @@ interface PocketProps {
 }
 
 export const Pocket: React.FC<PocketProps> = ({ color }) => {
-    const pieces: Pocket = {p: 0, n: 1, b: 1, r: 0, q: 1}; 
+    const pieces: Pocket = {p: 0, n: 2, b: 1, r: 0, q: 1}; 
 
     const handleDrag = (event: React.MouseEvent<HTMLDivElement>) => {
         event.preventDefault();
@@ -23,7 +23,16 @@ export const Pocket: React.FC<PocketProps> = ({ color }) => {
 
     return (
         <div className="pocket-container flex">
-            {['q', 'r', 'b', 'n', 'p'].map(piece => (<Image className={`${color}${piece} ${pieces[piece as keyof Pocket] > 0 ? "opacity-100" : "opacity-50"}`} key={`${color}${piece}`} onMouseDown={handleDrag} src={`/images/${color}${piece}.png`} width={75} height={75} alt={`${piece}`} priority></Image>))}
+            {['q', 'r', 'b', 'n', 'p'].map(piece => (
+            <div className="relative" key={`${color}${piece}`}>
+                <Image className={`${color}${piece} ${pieces[piece as keyof Pocket] > 0 ? "opacity-100" : "opacity-50"}`} key={`${color}${piece}`} onMouseDown={handleDrag} src={`/images/${color}${piece}.png`} width={75} height={75} alt={`${piece}`} priority></Image>
+                {pieces[piece as keyof Pocket] > 0 && <div className="flex items-align justify-center number-box absolute bottom-0 left-12 min-w-5 ring-4 ring-blue-600 outline outline-2 outline-blue-200 rounded-full bg-slate-50">
+                    <text>
+                        {pieces[piece as keyof Pocket]}
+                    </text>
+                </div>}
+            </div>
+            ))}
         </div>
     );
 };  
